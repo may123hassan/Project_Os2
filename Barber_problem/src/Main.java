96acbc1a6f02f3fc306cd6342b1cf7a076622237
may123hassan <105087587+may123hassan@users.noreply.github.com>
2022-12-19 00:23:53 +0200

    apply problem
    sleeping Barber problem

---
+++ Barber_problem/src/Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        new MyFrame();
+
+        //System.out.println("Hello world!");
+    }
+}
---
+++ Barber_problem/src/swingGui.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class swingGui {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button2;
+}
